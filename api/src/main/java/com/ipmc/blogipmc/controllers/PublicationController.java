e6301e53336ce88118ea924d1eddfaae5ae1da5d
Wesley Emanuel <wesleyemanuel.1517@gmail.com>
2024-02-21 11:13:55 -0300

    fix: treats CrossOrigin error

--- api/src/main/java/com/ipmc/blogipmc/controllers/PublicationController.java
+++ api/src/main/java/com/ipmc/blogipmc/controllers/PublicationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "publication")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class PublicationController {
     private final PublicationService publicationService;
     @GetMapping
