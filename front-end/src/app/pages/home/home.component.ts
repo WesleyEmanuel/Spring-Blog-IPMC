5db83ea53df0a9c14a66001250df0bc2037ff14a
Wesley Emanuel <wesleyemanuel.1517@gmail.com>
2024-04-02 15:58:03 -0300

    feat: gallery publications on home page and carousel for mobile

--- front-end/src/app/pages/home/home.component.ts
+++ front-end/src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HorariosComponent } from '../../components/horarios/horarios.component';
+import { PublicationsService } from '@app/services/publications/publications.service';
+import { Publication } from '@app/models/Publication';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +12,36 @@ import { HorariosComponent } from '../../components/horarios/horarios.component'
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit{
+
+  publications!: Publication[];
+  mobile_size: boolean = false;
+
+  constructor(private publicationsService: PublicationsService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkWindowWidth();
+    this.getPublications();
+  }
+  
+  getPublications(pageNumber?: number) {
+    this.publicationsService.getPublications(pageNumber).subscribe(
+      (resp) => {
+        this.publications = resp.content;
+      },
+    );
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWindowWidth();
+  }
+
+  checkWindowWidth() {
+    this.mobile_size = window.innerWidth <= 900;
+  }
+
+  openPublication(id: number) {
+    this.router.navigate(['publicacao'], { queryParams: { id } });
+  }
+}
